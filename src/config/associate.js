80779a02b0b46e491afda7647c0fab0d83c65886
HHDev <hoodhasko@yandex.ru>
2021-08-28 07:58:18 +0500

    Подправил конфиг

--- src/config/associate.js
+++ src/config/associate.js
@@ -1,26 +1,105 @@
 export const range = {
-  'b1.judge.isp1@gmail.com': 3, //D
-  'b1.judge.isp2@gmail.com': 4, //E
-  'b1.judge.isp3@gmail.com': 5, //F
-  'b1.judge.isp4@gmail.com': 6, //G
-  'b1.judge.art1@gmail.com': 8, //I
-  'b1.judge.art2@gmail.com': 9, //J
-  'b1.judge.art3@gmail.com': 10, //K
-  'b1.judge.art4@gmail.com': 11, //L
+  'b1.judge.isp1@gmail.com': {
+    indexScoreColumn: 3,
+    symbolScoreColumn: 'D',
+    decline: false,
+  }, //D
+  'b1.judge.isp2@gmail.com': {
+    indexScoreColumn: 4,
+    symbolScoreColumn: 'E',
+    decline: false,
+  }, //E
+  'b1.judge.isp3@gmail.com': {
+    indexScoreColumn: 5,
+    symbolScoreColumn: 'F',
+    decline: false,
+  }, //F
+  'b1.judge.isp4@gmail.com': {
+    indexScoreColumn: 6,
+    symbolScoreColumn: 'G',
+    decline: false,
+  }, //G
+  'b1.judge.art1@gmail.com': {
+    indexScoreColumn: 8,
+    symbolScoreColumn: 'I',
+    decline: false,
+  }, //I
+  'b1.judge.art2@gmail.com': {
+    indexScoreColumn: 9,
+    symbolScoreColumn: 'J',
+    decline: false,
+  }, //J
+  'b1.judge.art3@gmail.com': {
+    indexScoreColumn: 10,
+    symbolScoreColumn: 'K',
+    decline: false,
+  }, //K
+  'b1.judge.art4@gmail.com': {
+    indexScoreColumn: 11,
+    symbolScoreColumn: 'L',
+    decline: false,
+  }, //L
   'b1.judge.sl@gmail.com': {
     indexScoreColumn: 13,
     symbolScoreColumn: 'N',
     decline: true,
   }, //N col: 14 / Q col: 17
-  'b2.judge.isp1@gmail.com': 4,
-  'b2.judge.isp2@gmail.com': 5,
-  'b2.judge.isp3@gmail.com': 6,
-  'b2.judge.isp4@gmail.com': 7,
-  'b2.judge.art5@gmail.com': 9,
-  'b2.judge.art6@gmail.com': 10,
-  'b2.judge.art7@gmail.com': 11,
-  'b2.judge.art8@gmail.com': 12,
-  'b2.judge.sl@gmail.com': 14,
-  'bestscoreapp@gmail.com': {scoreColumn: 3, decline: false},
+
+  'b2.judge.isp1@gmail.com': {
+    indexScoreColumn: 3,
+    symbolScoreColumn: 'D',
+    decline: false,
+  }, //D
+  'b2.judge.isp2@gmail.com': {
+    indexScoreColumn: 4,
+    symbolScoreColumn: 'E',
+    decline: false,
+  }, //E
+  'b2.judge.isp3@gmail.com': {
+    indexScoreColumn: 5,
+    symbolScoreColumn: 'F',
+    decline: false,
+  }, //F
+  'b2.judge.isp4@gmail.com': {
+    indexScoreColumn: 6,
+    symbolScoreColumn: 'G',
+    decline: false,
+  }, //G
+  'b2.judge.art1@gmail.com': {
+    indexScoreColumn: 8,
+    symbolScoreColumn: 'I',
+    decline: false,
+  }, //I
+  'b2.judge.art2@gmail.com': {
+    indexScoreColumn: 9,
+    symbolScoreColumn: 'J',
+    decline: false,
+  }, //J
+  'b2.judge.art3@gmail.com': {
+    indexScoreColumn: 10,
+    symbolScoreColumn: 'K',
+    decline: false,
+  }, //K
+  'b2.judge.art4@gmail.com': {
+    indexScoreColumn: 11,
+    symbolScoreColumn: 'L',
+    decline: false,
+  }, //L
+  'b2.judge.sl@gmail.com': {
+    indexScoreColumn: 13,
+    symbolScoreColumn: 'N',
+    decline: true,
+  }, //N col: 14 / Q col: 17
+
+  // 'b2.judge.isp1@gmail.com': 4,
+  // 'b2.judge.isp2@gmail.com': 5,
+  // 'b2.judge.isp3@gmail.com': 6,
+  // 'b2.judge.isp4@gmail.com': 7,
+  // 'b2.judge.art5@gmail.com': 9,
+  // 'b2.judge.art6@gmail.com': 10,
+  // 'b2.judge.art7@gmail.com': 11,
+  // 'b2.judge.art8@gmail.com': 12,
+  // 'b2.judge.sl@gmail.com': 14,
+  // 'bestscoreapp@gmail.com': {scoreColumn: 3, decline: false},
   // 'bestscoreapp@gmail.com': [5, 17],
 };
