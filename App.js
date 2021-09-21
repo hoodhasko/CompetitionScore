37291f600ac594900f330b2c0b38e2071c9565bd
HHDev <hoodhasko@yandex.ru>
2021-09-21 11:22:19 +0500

    Исправлен баг, при котором после разворачивания приложения отображалась навигационная панель

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {hideNavigationBar} from 'react-native-navigation-bar-color';
+import {useAppState} from '@react-native-community/hooks';
 
 import Navigation from './src/navigation';
 
 const App = () => {
-  hideNavigationBar();
+  const currentAppState = useAppState();
+  useEffect(() => {
+    if (currentAppState === 'active') {
+      hideNavigationBar();
+    }
+  }, [currentAppState]);
 
   return <Navigation />;
 };
